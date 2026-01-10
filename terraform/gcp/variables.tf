ea3efe541caac27144eb6f26fdfef0768ad47d2e
sadafp-ops <sadaf.p@turing.com>
2026-01-10 18:33:07 +0530

    test: staging cd trigger

--- terraform/gcp/variables.tf
+++ terraform/gcp/variables.tf
@@ -1,16 +1,19 @@
 variable "project_id" {
   description = "GCP Project ID"
+  type        = string
 }
 
 variable "region" {
   description = "GCP Region"
-  default     = "asia-south1"
+  type        = string
 }
 
 variable "backend_image" {
-  description = "Docker Hub backend image"
+  description = "Backend image URI"
+  type        = string
 }
 
 variable "frontend_image" {
-  description = "Docker Hub frontend image"
+  description = "Frontend image URI"
+  type        = string
 }
