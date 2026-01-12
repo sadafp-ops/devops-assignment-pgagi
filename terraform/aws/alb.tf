5973c2c1cde800dfc541dd6b4d8c38ec948cfadb
sadafp-ops <sadaf.p@turing.com>
2026-01-12 17:47:54 +0530

    fix: correct deploy pipeline step structure

--- terraform/aws/alb.tf
+++ terraform/aws/alb.tf
@@ -1,61 +1,43 @@
-####################################
-# APPLICATION LOAD BALANCER
-####################################
 resource "aws_lb" "app" {
   name               = "assignment-alb"
   load_balancer_type = "application"
   internal           = false
-  subnets            = data.aws_subnets.public.ids
+
+  security_groups = [aws_security_group.alb.id]
+
+  subnets = [
+    aws_subnet.public_1a.id,
+    aws_subnet.public_1b.id
+  ]
 }
 
-####################################
-# HTTP LISTENER
-####################################
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.app.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      status_code  = "404"
-      content_type = "text/plain"
-      message_body = "Not Found"
-    }
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend.arn
   }
 }
 
-####################################
-# BACKEND TARGET GROUP
-####################################
 resource "aws_lb_target_group" "backend" {
   name        = "backend-tg"
   port        = 8080
   protocol    = "HTTP"
   target_type = "ip"
-  vpc_id      = data.aws_vpc.main.id
-
-  health_check {
-    path = "/health"
-  }
+  vpc_id      = aws_vpc.main.id
 }
 
-####################################
-# FRONTEND TARGET GROUP
-####################################
 resource "aws_lb_target_group" "frontend" {
   name        = "frontend-tg"
   port        = 3000
   protocol    = "HTTP"
   target_type = "ip"
-  vpc_id      = data.aws_vpc.main.id
+  vpc_id      = aws_vpc.main.id
 }
 
-####################################
-# LISTENER RULES
-####################################
 resource "aws_lb_listener_rule" "backend" {
   listener_arn = aws_lb_listener.http.arn
   priority     = 10
